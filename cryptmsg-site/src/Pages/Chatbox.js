cb3becf9cd8f08cb3ef3256536b7393da1dee7f5
jacob-hawkins <jacob.hawkins010@gmail.com>
2022-10-10 17:12:11 -0400

    Fixed Chatbox

--- cryptmsg-site/src/Pages/Chatbox.js
+++ cryptmsg-site/src/Pages/Chatbox.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import './Chatbox.css'
+import Message from './Message';
+
+function sendText() {
+    var text = document.getElementById("input-box").value;
+
+    return text;
+}
+
 
 function Chatbox() {
-  return (
-    <div className='chatbox'>
-        <div>
-            <div id="input-box" contentEditable />
-            <button id="input-submit">&#9650;</button>
+    return (
+        <div className='chatbox'>
+            <div>
+                <div id="input-box" contentEditable />
+                <button id="input-submit" onClick={sendText()}>&#9650;</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Chatbox
