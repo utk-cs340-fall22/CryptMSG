f77a5753e5b4782b84c1bc2a5801fc18a5d4494e
unknown <rileyjcrockett@gmail.com>
2022-10-11 18:40:05 -0400

    Added dynamic message creation and enter key functionality for submitting messages.

--- cryptmsg-site/src/Pages/Chatbox.js
+++ cryptmsg-site/src/Pages/Chatbox.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import './Chatbox.css'
 
-function sendText() {
+function SendText() {
     var text = document.getElementById("input-box").innerHTML;
 
     if (text.length > 0) {
-        document.getElementById("message-board").innerHTML += `<div>${text}</div>`;
+        var msg = document.createElement("div");
+        msg.className = "messages";
+        msg.innerHTML = text;
+        document.getElementById("message-board").appendChild(msg);
     }
 
     document.getElementById("input-box").innerHTML = "";
+}
+
+var EnterInput = function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        SendText();
+    }
 }
 
 function Chatbox() { 
     return (
         <div className='chatbox'>
             <div>
-                <div id="input-box" contentEditable />
-                <button id="input-submit" onClick={sendText}>&#9650;</button>
+                <div id="input-box" onKeyDown={EnterInput} contentEditable />
+                <button id="input-submit" onClick={SendText}>&#9650;</button>
             </div>
         </div>
     )
