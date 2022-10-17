bd6d72989db00537b54d0e116826c51119fe3b3e
jacob-hawkins <jacob.hawkins010@gmail.com>
2022-10-17 18:44:03 -0400

    smooth scroll on send

--- cryptmsg-site/src/Pages/Chatbox.js
+++ cryptmsg-site/src/Pages/Chatbox.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import './Chatbox.css'
 
+const scrollToBottom = () =>{ 
+    window.scrollTo({ 
+      top: document.documentElement.scrollHeight, 
+      behavior: 'smooth'
+    }); 
+  }; 
+
 function SendText() {
     var text = document.getElementById("input-box").innerHTML;
 
     if (text.length > 0) {
         var msg = document.createElement("div");
         msg.className = "messages";
+        msg.id = "sent"
         msg.innerHTML = text;
         document.getElementById("message-board").appendChild(msg);
     }
 
     document.getElementById("input-box").innerHTML = "";
+    scrollToBottom();
 }
 
 var EnterInput = function(event) {
