315db07e9e9e543b278396fd52e16ada92ccdcc1
Reagan Austin <raustin9@vols.utk.edu>
2022-10-18 17:11:57 -0400

    textbox now auto resizes its height

--- cryptmsg-site/src/Pages/Chatbox.js
+++ cryptmsg-site/src/Pages/Chatbox.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Component } from 'react'
 import './Chatbox.css'
 import * as FaIcons from 'react-icons/fa'
 import { IconContext } from 'react-icons/lib'
+import autosize from 'autosize';
+
 
 const scrollToBottom = () =>{ 
     window.scrollTo({ 
@@ -11,7 +14,7 @@ const scrollToBottom = () =>{
 }; 
 
 function SendText() {
-    var text = document.getElementById("input-box").innerHTML;
+    var text = document.getElementById("input-box").value;
     var height = document.getElementById("message-board").clientHeight;
 
     if (text.length > 0) {
@@ -20,6 +23,7 @@ function SendText() {
         msg.id = "sent"
         msg.innerHTML = text;
         document.getElementById("message-board").prepend(msg);
+        document.getElementById("input-box").value = "";
     }
 
     document.getElementById("input-box").innerHTML = "";
@@ -37,21 +41,29 @@ var EnterInput = function(event) {
     }
 }
 
-function Chatbox() {     
-    return (
-        <div className='chatbox'>
-            <div>
-                <div id="input-box" onKeyDown={EnterInput} contentEditable />
-                {/* <button id="input-submit" onClick={SendText}>&#9650;</button> */}
-                <IconContext.Provider value={{ className: 'input-submit-icon' }}>
-                    <button id="input-submit" onClick={SendText}>
-                        {/* &#9650; */}
-                        <FaIcons.FaPaperPlane />
-                    </button>
-                </IconContext.Provider>
+
+
+class Chatbox extends Component {   
+    componentDidMount(){
+        // This causes the text area to autoresize when needing more or less lines of input
+        const tarea = document.getElementById("input-box");
+        autosize(tarea);
+    } 
+
+    render() {
+        return (
+            <div className='chatbox'>
+                <div>
+                    <textarea id="input-box" onKeyDown={EnterInput} rows="1" cols="50" wrap="soft"/>
+                    <IconContext.Provider value={{ className: 'input-submit-icon' }}>
+                        <button id="input-submit" onClick={SendText}>
+                            <FaIcons.FaPaperPlane />
+                        </button>
+                    </IconContext.Provider>
+                </div>
             </div>
-        </div>
-    )
+        );
+    } 
 }
 
 export default Chatbox
