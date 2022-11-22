04828cc6415bfe9a13c58747fa9071ec76dd9c18
Jacob Hawkins <jacobhawkins@jacobmac2.nomads.utk.edu>
2022-11-22 09:59:38 -0500

    bug

--- cryptmsg-site/src/App.js
+++ cryptmsg-site/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Messaging from './pages/Messaging';
 
