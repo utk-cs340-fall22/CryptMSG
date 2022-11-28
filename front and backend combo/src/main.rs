d8f5f1c0ebe5987cff6a7194ef73a0e1a01026af
Kijan Daniel <termin0001@gmail.com>
2022-11-28 18:27:09 -0500

    working server code

--- front and backend combo/src/main.rs
+++ front and backend combo/src/main.rs
@@ -6,35 +6,16 @@ use rocket::{State, Shutdown};
 use rocket::fs::{relative, FileServer};
 use rocket::form::Form;
 use rocket::response::stream::{EventStream, Event};
-use rocket::serde::{Serialize, Deserialize};
+use rocket::serde::{json::Json, Serialize, Deserialize};
 use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
 use rocket::tokio::select;
-use std::io;
-use std::path::{Path, PathBuf};
-use rocket_contrib::serve::StaticFiles;
-use rocket::response::{NamedFile};
-/*use std::{io, path::{Path, PathBuf}};
-use rocket::{get, routes, response::{NamedFile, Redirect}};
 
-#[get("/")]
-fn index() -> Redirect {
-    Redirect::permanent("/index.html")
-}
-
-#[get("/<file..>")]
-fn build_dir(file: PathBuf) -> io::Result<NamedFile> {
-    NamedFile::open(Path::new("build/").join(file))
-}
-*/
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
 #[serde(crate = "rocket::serde")]
 struct Message {
-    #[field(validate = len(..30))]
-    pub room: String,
     #[field(validate = len(..20))]
-    pub username: String,
-    pub message: String,
+    pub message: String
 }
 
 /// Returns an infinite stream of server-sent events. Each event is a message
@@ -59,26 +40,19 @@ async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStrea
 }
 
 /// Receive a message from a form submission and broadcast it to any receivers.
-#[post("/message", data = "<form>")]
-fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+// #[post("/message", data = "<form>")]
+#[post("/message", data = "<json_data>")]
+fn post(json_data: Json<Message>, queue: &State<Sender<Message>>) {
     // A send 'fails' if there are no active subscribers. That's okay.
-    let _res = queue.send(form.into_inner());
-}
-
-#[get("/<file..>")]
-    fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("build/").join(file)).ok()
-}
-#[get("/")]
-    fn index() -> io::Result<NamedFile> {
-    NamedFile::open("build/index.html")
+    let message = json_data.into_inner();
+    println!("content! {:?}", message.message);
+    let _res = queue.send(message);
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
-        .mount("/", routes![post, events,files])
-        //.mount("/", FileServer::from(relative!("static")))
-        //.mount("/", routes![index, build_dir])
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("build")))
 }
