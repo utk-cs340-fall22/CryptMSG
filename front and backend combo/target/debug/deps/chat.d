28f1cb64abe32b6af7cd981f8f05bf4e0831c487
Kijan Daniel <termin0001@gmail.com>
2022-11-28 18:32:40 -0500

    build in front end backend

--- front and backend combo/target/debug/deps/chat.d
+++ front and backend combo/target/debug/deps/chat.d
@@ -1,5 +1,7 @@
-C:\Users\termi\CryptMSG\cryptmsg-site\target\debug\deps\chat.exe: src\main.rs
+C:\Users\termi\CryptMSG\front and backend combo\target\debug\deps\chat.exe: src\main.rs
 
-C:\Users\termi\CryptMSG\cryptmsg-site\target\debug\deps\chat.d: src\main.rs
+C:\Users\termi\CryptMSG\front and backend combo\target\debug\deps\chat.d: src\main.rs
 
 src\main.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=C:\\Users\\termi\\CryptMSG\\front and backend combo
